37b18cce773f1cb1ff98df7546fbe50d76796401
techiemmk <techiemmk@gmail.com>
2018-04-09 18:51:23 +0530

    added Procfile

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
+var PORT = process.env.PORT || 2020;
+
 http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type': 'text/html'});
 	response.end('<html><head><meta charset="utf-8" /></head><body>Hello Maghin !!!</body></html>');
 	console.log('Hello from Maghin !!!');
-}).listen(2020);
+}).listen(PORT);
